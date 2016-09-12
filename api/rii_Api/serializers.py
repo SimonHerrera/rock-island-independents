e32926d50a3309696615276c821072a6be0057fb
Simon Herrera <simonherrera.tech@gmail.com>
2016-09-12 15:49:19 -0500

    Api Working - Need to add Images

--- api/rii_Api/serializers.py
+++ api/rii_Api/serializers.py
@@ -5,29 +5,29 @@ from rest_framework import serializers
 class GameSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Game
-        fields = ('id', 'url', 'attendance')
+        fields = ('id', 'url', 'attendance', 'week', 'result', 'rockIslandScore', 'opponentScore', 'rii1st', 'rii2nd', 'rii3rd', 'rii4th', 'opp1st', 'opp2nd', 'opp3rd', 'opp4th', 'gameSummary', 'yearId', 'opponentId', 'locationId', 'date')
 
 
 class YearSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Year
-        fields = ('id', 'url', 'year')
+        fields = ('id', 'url', 'year', 'wins', 'losses', 'ties', 'yearSummary', 'managerId', 'coachId') #add image
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Player
-        fields = ('id', 'url', 'position')
+        fields = ('id', 'url', 'season', 'nickName', 'firstName', 'lastName', 'legalName', 'position', 'height', 'weight', 'birthDate', 'birthCity', 'birthState', 'birthCountry', 'college', 'playerBio')
 
 class LocationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Location
-        fields = ('id', 'url', 'venueName')
+        fields = ('id', 'url', 'city', 'state', 'venueName', 'games')
 
 class OpponentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Opponent
-        fields = ('id', 'url', 'name')
+        fields = ('id', 'url', 'name', 'games')
 
 class CoachSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
