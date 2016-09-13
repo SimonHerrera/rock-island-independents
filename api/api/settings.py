e9329acb3240f48323da0c2468895a0e147a72be
Simon Herrera <simonherrera.tech@gmail.com>
2016-09-13 10:32:42 -0500

    Player Images Working

--- api/api/settings.py
+++ api/api/settings.py
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = "C:\\Users\\Simon Herrera\\workspace\\python\\rock-island-independents\\api"
+MEDIA_URL = '/media/'
